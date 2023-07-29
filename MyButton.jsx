49a10a076e804abaa95115490731e866149cc75e
Brahim  Maaqoul <wacakoule@gmail.com>
2023-07-29 10:59:10 +0100

    officially starting react

---
+++ MyButton.jsx
@@ -0,0 +1,10 @@
+function MyButton(){
+    return (
+        <div>
+            <h1>yoo</h1>
+            <button>I'm a button</button>
+        </div>
+    )
+}
+
+ReactDOM.render(<MyButton/>, document.querySelector("body"));
---
+++ img.jsx
@@ -0,0 +1,25 @@
+const   user = {
+    name : 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90
+};
+
+function Profile(){
+    return (
+        <>
+            <h1>{user.name}</h1>
+            <img
+                src={user.imageUrl}
+                alt={'Photo of ' + user.name}
+                style={
+                    {
+                        width: user.imageSize,
+                        height:user.imageSize
+                    }
+                }
+            />
+        </>
+    )
+}
+
+ReactDOM.render(<Profile/>, document.querySelector("body"));
